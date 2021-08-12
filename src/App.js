61cc82e5b230b48197f344f500283a61816bad61
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2021-08-12 10:47:45 +0530

    Latest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import React from "react";
 import "react-dropdown/style.css";
+import Card from "@material-ui/core/Card";
+import { CardContent } from "@material-ui/core";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -41,7 +43,8 @@ class App extends React.Component {
       list1,
       list2,
       list3,
-      newItem: ""
+      newItem: "",
+      newItemType: ""
     });
   }
 
@@ -76,122 +79,153 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1 className="app-title">MY LIST</h1>
+      <>
+        <Card>
+          <h1 className="app-title">MY LIST</h1>
 
-        <div className="container">
-          <div
-            style={{
-              padding: 30,
-              textAlign: "left",
-              maxWidth: 500,
-              margin: "auto"
-            }}
-          >
-            Add an Item...
-            <br />
-            <input
-              type="text"
-              placeholder="Type item here"
-              value={this.state.newItem}
-              onChange={(e) => this.updateInput("newItem", e.target.value)}
-            />
-            <label>
-              <select
-                name="type"
-                value={this.state.newItemType}
-                onChange={(e) =>
-                  this.updateInput("newItemType", e.target.value)
-                }
-              >
-                <option value="personal">Personal</option>
-                <option value="office">Office</option>
-                <option value="shopping">Shopping</option>
-              </select>
-            </label>
-            <button
-              className="add-btn btn-floating"
-              onClick={() => this.addItem()}
-              disabled={!this.state.newItem.length}
+          <div className="container">
+            <div
+              style={{
+                padding: 30,
+                textAlign: "left",
+                maxWidth: 500,
+                margin: "auto"
+              }}
             >
-              <i class="material-icons"> + </i>
-            </button>
-            <br /> <br />
-            <div>
-              <h2>ALL</h2>
-              <ul>
-                {this.state.list.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      {item.value} {item.type}
-                      <button
-                        className="btn btn-floating"
-                        onClick={() => this.deleteItem(item.id)}
-                      >
-                        <i class="material-icons">x</i>
-                      </button>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div>
-              <h2>Personal</h2>
-              <ul>
-                {this.state.list1.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      {item.value} {item.type}
-                      <button
-                        className="btn btn-floating"
-                        onClick={() => this.deleteItem1(item.id)}
-                      >
-                        <i class="material-icons">x</i>
-                      </button>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div>
-              <h2>Office</h2>
-              <ul>
-                {this.state.list2.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      {item.value} {item.type}
-                      <button
-                        className="btn btn-floating"
-                        onClick={() => this.deleteItem2(item.id)}
-                      >
-                        <i class="material-icons">x</i>
-                      </button>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div>
-              <h2>Shopping</h2>
-              <ul>
-                {this.state.list3.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      {item.value} {item.type}
-                      <button
-                        className="btn btn-floating"
-                        onClick={() => this.deleteItem3(item.id)}
-                      >
-                        <i class="material-icons">x</i>
-                      </button>
-                    </li>
-                  );
-                })}
-              </ul>
+              Add an Item...
+              <br />
+              <input
+                type="text"
+                placeholder="Type item here"
+                value={this.state.newItem}
+                onChange={(e) => this.updateInput("newItem", e.target.value)}
+              />
+              <label>
+                <select
+                  name="type"
+                  value={this.state.newItemType}
+                  onChange={(e) =>
+                    this.updateInput("newItemType", e.target.value)
+                  }
+                >
+                  <option value=""></option>
+                  <option value="personal">Personal</option>
+                  <option value="office">Office</option>
+                  <option value="shopping">Shopping</option>
+                </select>
+              </label>
+              <button
+                className="add-btn btn-floating"
+                onClick={() => this.addItem()}
+                disabled={!this.state.newItem.length}
+              >
+                <i class="material-icons"> + </i>
+              </button>
+              <br /> <br />
+              <div>
+                <CardContent>
+                  <h2>ALL</h2>
+                  <ul>
+                    {this.state.list.map((item) => {
+                      return (
+                        <li key={item.id}>
+                          {item.value} {item.type}
+                          <button
+                            className="btn btn-floating"
+                            onClick={() => {
+                              if (item.type === "personal") {
+                                this.deleteItem(item.id);
+                                this.deleteItem1(item.id);
+                              } else if (item.type === "office") {
+                                this.deleteItem(item.id);
+                                this.deleteItem2(item.id);
+                              } else if (item.type === "shopping") {
+                                this.deleteItem(item.id);
+                                this.deleteItem3(item.id);
+                              }
+                            }}
+                          >
+                            <i class="material-icons">x</i>
+                          </button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </CardContent>
+              </div>
+              <div>
+                <CardContent>
+                  <h2>Personal</h2>
+                  <ul>
+                    {this.state.list1.map((item) => {
+                      return (
+                        <li key={item.id}>
+                          {item.value} {item.type}
+                          <button
+                            className="btn btn-floating"
+                            onClick={() => {
+                              this.deleteItem1(item.id);
+                              this.deleteItem(item.id);
+                            }}
+                          >
+                            <i class="material-icons">x</i>
+                          </button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </CardContent>
+              </div>
+              <div>
+                <CardContent>
+                  <h2>Office</h2>
+                  <ul>
+                    {this.state.list2.map((item) => {
+                      return (
+                        <li key={item.id}>
+                          {item.value} {item.type}
+                          <button
+                            className="btn btn-floating"
+                            onClick={() => {
+                              this.deleteItem2(item.id);
+                              this.deleteItem(item.id);
+                            }}
+                          >
+                            <i class="material-icons">x</i>
+                          </button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </CardContent>
+              </div>
+              <div>
+                <CardContent>
+                  <h2>Shopping</h2>
+                  <ul>
+                    {this.state.list3.map((item) => {
+                      return (
+                        <li key={item.id}>
+                          {item.value} {item.type}
+                          <button
+                            className="btn btn-floating"
+                            onClick={() => {
+                              this.deleteItem3(item.id);
+                              this.deleteItem(item.id);
+                            }}
+                          >
+                            <i class="material-icons">x</i>
+                          </button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </CardContent>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </Card>
+      </>
     );
   }
 }
